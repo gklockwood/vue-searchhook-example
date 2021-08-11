f67507fd306c5ab337d1b638db7c1949996495dd
Galen Lockwood <galenlockwood@gmail.com>
2021-08-11 17:21:08 -0400

    Initial commit

---
+++ src/hooks/search.js
@@ -0,0 +1,38 @@
+import { ref, computed, watch } from 'vue';
+
+// Using the term items to make it reusable 
+// adding parameter of searchProp to store dynamic values
+export default function useSearch(items, searchProp) {
+    const enteredSearchTerm = ref('');
+    const activeSearchTerm = ref('');
+
+    const availableItems = computed(function() {
+        let filteredItems = [];
+        if (activeSearchTerm.value) {
+            filteredItems = items.filter((item) =>
+                item[searchProp].toLowerCase().includes(activeSearchTerm.value.toLowerCase())
+            );
+        } else if (items) {
+            filteredItems = items;
+        }
+        return filteredItems;
+    });
+
+    watch(enteredSearchTerm, function(newValue) {
+        setTimeout(() => {
+            if (newValue === enteredSearchTerm.value) {
+                activeSearchTerm.value = newValue;
+            }
+        }, 300);
+    });
+
+    function updateSearch(val) {
+        enteredSearchTerm.value = val;
+    }
+
+    return {
+        enteredSearchTerm,
+        availableItems,
+        updateSearch
+    };
+}
